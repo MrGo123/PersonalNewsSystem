65f82c0e2289e2cb6f786989d6917c65c6a0d5a7
MrGo123 <zymail68@foxmail.com>
2020-12-27 11:04:55 +0800

    build(init):first commit

---
+++ PersonalNews/Controllers/lanmuController.cs
@@ -0,0 +1,75 @@
+﻿using PersonalNews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalNews.Controllers
+{
+    public class lanmuController : Controller
+    {
+        userDBContext db = new userDBContext();
+        //
+        // GET: lanmu
+
+       public ActionResult Index()
+        {
+            return View("index");
+        }
+        //文章分页
+        public ActionResult GetDivideList()
+        {
+            string s = "";
+            foreach (Category c in db.Categorys)
+            {
+                //onclick="getnews(1)"
+                s += "<li id='lanmu1' onclick='getDivideNews(" + c.CategoryId + ",1)' > " + c.CategoryName + "</li>";
+            }
+            return Content(s);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category c)
+        {
+            db.Categorys.Add(c);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Category ca = db.Categorys.Find(id);
+            if (ca == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ca);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            Category ca = db.Categorys.Find(id);
+            db.Categorys.Remove(ca);
+            db.SaveChanges();
+            return View();
+        }
+        
+        public ActionResult Details(int? id) //删除的时候提交的数据只是id 和修改不一样
+        {
+            Category category = db.Categorys.Find(id); //找到
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category); //放到视图里访问
+        }
+
+    }
+}
---
+++ PersonalNews/Models/User.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PersonalNews.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        /// </summary>
+    }
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Display(Name="专栏名称")]
+        public string CategoryName { get; set; }
+    }
+
+    public class Article
+    {
+        [Key]
+        public int ArticleId { get; set; }
+        /// <summary>
+        /// 栏目id
+        /// </summary>
+        [Display(Name = "栏目ID")]
+        [Required(ErrorMessage = "×")]
+        public int CategoryId { get; set; }
+        /// <summary>
+        /// 来源
+        /// </summary>
+        [Display(Name = "来源")]
+        [StringLength(255, ErrorMessage = "×")]
+        public string Source { get; set; }
+        /// <summary>
+        /// 作者
+        /// </summary>
+        [Display(Name = "作者")]
+        [StringLength(50, ErrorMessage = "×")]
+        public string Author { get; set; }
+        /// <summary>
+        /// 摘要
+        /// </summary> [NotMapped]
+        [Display(Name = "摘要")]
+        public string Intro { get; set; }
+        /// <summary>
+        /// 内容
+        /// </summary>
+        [Display(Name = "内容")]
+        [Required(ErrorMessage = "×")]
+        [DataType(DataType.Html)]
+        public string Content { get; set; }
+    }
+    public class userDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<Article> Articles { get; set; }
+    }
+}
